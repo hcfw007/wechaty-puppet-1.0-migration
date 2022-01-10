26a3e2bc6812f3a4c57817b3c5e2cdfa500fb694
NickWang <brotherstyx@gmail.com>
2022-01-10 14:02:32 +0800

    feat: 🎸 types

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import { 
+  ContactGender,
+  Contact,
+  FriendshipScene,
+  Friendship,
+  Image,
+  Message,
+  Payload,
+} from 'wechaty-puppet/types'
+
+export {
+  ContactGender,
+  Contact as ContactType,
+  FriendshipScene as FriendshipSceneType,
+  Friendship as FriendshipType,
+  Image as ImageType,
+  Message as MessageType,
+  Payload as PayloadType,
+}
