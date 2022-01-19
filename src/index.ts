6a39786278dd543607fca99626c5bc501a22b682
NickWang <brotherstyx@gmail.com>
2022-01-19 16:16:16 +0800

    refactor: 💡 folder structure

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,6 @@ export {
 } from 'wechaty-puppet'
 
 export type { PuppetOptions } from 'wechaty-puppet'
-export * from './src/helpers'
-export * from './src/payloads'
-export * from './src/types'
+export * from './modules/helpers'
+export * from './modules/payloads'
+export * from './modules/types'
